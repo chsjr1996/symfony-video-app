92984d1aa41e19e8d6f76c4224f5997b29acfaf9
Carlos Henrique <chsjr1996@gmail.com>
2022-05-07 11:40:17 -0300

    feat: add Doctrine ORM, Fixture and some Entities
    - add packages to manage DB (doctrine ORM, fixtures)
    - add Category and Video entities
    - add CategoryFixtures
    - update docker environment to support PSQL DBMS

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $this->loadMainCategories($manager);
+    }
+
+    private function loadMainCategories(ObjectManager $manager): void
+    {
+        foreach ($this->getMainCategoriesData() as [$name]) {
+            $category = new Category();
+            $category->setName($name);
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+
+    private function getMainCategoriesData(): array
+    {
+        return [
+            ['Electronics', 1],
+            ['Toys', 2],
+            ['Books', 3],
+            ['Movies', 4],
+        ];
+    }
+}

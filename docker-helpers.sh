632dc14679d4a3a03bc13978b1a8d979b7a5f074
Carlos Henrique <chsjr1996@gmail.com>
2022-05-07 12:53:10 -0300

    feat: add 'docker-helpers' shell script
    This script contains some helpful shortcuts

---
+++ docker-helpers.sh
@@ -0,0 +1,41 @@
+# !/bin/sh
+
+function clear_database() {
+  read -p "Are you sure? [y/n]: " -n 1 -r
+
+  if [ "$REPLY" != "y" ]; then
+    echo
+    echo "Canceled!"
+    exit
+  fi
+
+  echo
+  echo "Cleaning database"
+  docker-compose exec php ./bin/console doctrine:schema:drop -n -q --force --full-database
+
+  echo "Cleaning migrations"
+  rm -rf migrations/*.php
+
+  echo "Creating migrations"
+  docker-compose exec php ./bin/console make:migration -n -q
+
+  echo "Running migrations"
+  docker-compose exec php ./bin/console doctrine:migrations:migrate -n -q
+
+  echo "Running fixtures"
+  docker-compose exec php ./bin/console doctrine:fixtures:load -n -q
+}
+
+
+case $1 in
+    --rebuild-db)
+        clear_database
+        ;;
+    --console)
+        docker-compose exec php ./bin/console
+        ;;
+    *)
+        echo "Options are: '--rebuild-db', '--console'"
+        ;;
+esac
+
--- docker_symfony_console.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-docker exec -it symfony_todo_sample-php-1 bin/console $1

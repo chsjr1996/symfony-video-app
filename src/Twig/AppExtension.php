dcd79cd6600a7140cbf2502972646298893b5cf2
Carlos Henrique <chsjr1996@gmail.com>
2022-05-08 11:27:54 -0300

    feat: remove unnecessary comments on Twig/AppExtension

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -11,9 +11,6 @@ class AppExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            // If your filter generates SAFE HTML, you should add a third
-            // parameter: ['is_safe' => ['html']]
-            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
             new TwigFilter('slugify', [$this, 'slugify']),
         ];
     }
